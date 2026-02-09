2e972482df6c6401ddd8b3bcb6df88e5f05e59e1
abreuwallace <abreu.encgb@poli.ufrj.br>
2026-02-09 12:58:30 -0300

    update train config

--- train.py
+++ train.py
@@ -174,7 +174,7 @@ def _main(args):
     wandb.finish()
 
 
-@hydra.main(config_path="conf", config_name="main_config_ps")  # for latest version of hydra=1.0
+@hydra.main(config_path="conf", config_name="main_config")  # for latest version of hydra=1.0
 def main(args):
     try:
         _main(args)
